808ca8f1261156e7e7cd571e7581173121f5a47c
andy-0101 <liandy0525@gmail.com>
2026-07-20 21:22:18 +0800

    将代码格式规范

--- src/main/java/dev/kikugie/techutils/config/LitematicConfigs.java
+++ src/main/java/dev/kikugie/techutils/config/LitematicConfigs.java
@@ -6,44 +6,100 @@ import fi.dy.masa.malilib.config.options.*;
 import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 
 public class LitematicConfigs extends Configs.BaseConfigs {
-	public static final ConfigBoolean RENDER_PREVIEW = new ConfigBoolean("renderPreview", true,
-		"Show 3D render of selected litematic in Load Schematics menu\n(Works only for .litematic files)");
-	public static final ConfigBoolean OVERRIDE_PREVIEW = new ConfigBoolean("overridePreview", false,
-		"Show 3D render even if litematic has its own preview");
-	public static final ConfigOptionList RENDER_ROTATION_MODE = new ConfigOptionList("rotationMode", InteractionProfiles.DRAG,
-		"""
+	public static final ConfigBoolean RENDER_PREVIEW = new ConfigBoolean(
+			"techutils.config.litematica.render_preview",
+			true,
+			"Show 3D render of selected litematic in Load Schematics menu\n(Works only for .litematic files)"
+		);
+	public static final ConfigBoolean OVERRIDE_PREVIEW = new ConfigBoolean(
+			"overridePreview",
+			false,
+			"Show 3D render even if litematic has its own preview"
+	);
+	public static final ConfigOptionList RENDER_ROTATION_MODE = new ConfigOptionList(
+			"rotationMode", InteractionProfiles.DRAG,
+			"""
 			Configure model rotation mode:
 			- DRAG: drag left mouse button in the viewport to rotate the model, drag right mouse button to pan the camera and scroll to zoom;
 			- POSITION: rotation follows horizontal mouse position on the screen;
-			- SPIN: rotate model at constant speed.""");
-	public static final ConfigDouble ROTATION_FACTOR = new ConfigDouble("rotationFactor", 1, 0.1, 10,
-		"Set model rotation sensitivity");
-	public static final ConfigInteger RENDER_SLANT = new ConfigInteger("renderSlant", 30, 0, 60,
-		"Set model vertical slant");
-	public static final ConfigHotkey ROTATE_PLACEMENT = new ConfigHotkey("rotatePlacement", "R",
-		"Rotate selected placement clockwise");
-	public static final ConfigHotkey MIRROR_PLACEMENT = new ConfigHotkey("mirrorPlacement", "Y",
-		"Cycle through selected placement's mirroring options");
-	public static final ConfigBooleanHotkeyed INVENTORY_SCREEN_OVERLAY = new ConfigBooleanHotkeyed("inventoryScreenOverlay", true, "I, O", KeybindSettings.GUI, """
+			- SPIN: rotate model at constant speed."""
+	);
+	public static final ConfigDouble ROTATION_FACTOR = new ConfigDouble(
+			"rotationFactor",
+			1,
+			0.1,
+			10,
+			"Set model rotation sensitivity"
+	);
+	public static final ConfigInteger RENDER_SLANT = new ConfigInteger(
+			"renderSlant",
+			30,
+			0,
+			60,
+			"Set model vertical slant"
+	);
+	public static final ConfigHotkey ROTATE_PLACEMENT = new ConfigHotkey(
+			"rotatePlacement",
+			"R",
+			"Rotate selected placement clockwise"
+	);
+	public static final ConfigHotkey MIRROR_PLACEMENT = new ConfigHotkey(
+			"mirrorPlacement",
+			"Y",
+			"Cycle through selected placement's mirroring options"
+	);
+	public static final ConfigBooleanHotkeyed INVENTORY_SCREEN_OVERLAY = new ConfigBooleanHotkeyed(
+			"inventoryScreenOverlay",
+			true,
+			"I,O",
+			KeybindSettings.GUI,
+			"""
 		Show layout of the container according to the litematic placement.
 		Item colors match your placement block colors. By default its:
 		- Light blue: missing item;
 		- Orange: mismatched amount or nbt data;
 		- Magenta: extra item that shouldn't be present;
-		- Red: wrong item type.""");
-	public static final ConfigHotkey REFRESH_MATERIAL_LIST = new ConfigHotkey("refreshMaterialList", "", """
+		- Red: wrong item type."""
+	);
+	public static final ConfigHotkey REFRESH_MATERIAL_LIST = new ConfigHotkey(
+			"refreshMaterialList",
+			"",
+			"""
 		Refreshes active Litematica material list.
-		§7This can be used in combination with layered lists to update the HUD when changing the active layer.""");
-	public static final ConfigBooleanHotkeyed EASY_PLACE_FULL_BLOCKS = new ConfigBooleanHotkeyed("easyPlaceFullBlocks", false, "", """
+		§7This can be used in combination with layered lists to update the HUD when changing the active layer."""
+	);
+	public static final ConfigBooleanHotkeyed EASY_PLACE_FULL_BLOCKS = new ConfigBooleanHotkeyed(
+			"easyPlaceFullBlocks",
+			false,
+			"",
+			"""
 		Treat all blocks as full cubes when using Litematica's easy place feature.
-		§7Useful for placing blocks with small hitboxes like buttons, chains, fences, etc.""");
-	public static final ConfigBooleanHotkeyed VERIFY_ITEM_COMPONENTS = new ConfigBooleanHotkeyed("verifyItemComponents", false, "", KeybindSettings.GUI, """
+		§7Useful for placing blocks with small hitboxes like buttons, chains, fences, etc."""
+	);
+	public static final ConfigBooleanHotkeyed VERIFY_ITEM_COMPONENTS = new ConfigBooleanHotkeyed(
+			"verifyItemComponents",
+			false,
+			"",
+			KeybindSettings.GUI,
+			"""
 		Make the inventory verifier enforce exact item component matches.
-		§7Also adds the present components to the lore!""");
-	public static final ConfigBooleanHotkeyed REPLACE_ITEM_PREDICATES_WITH_PLACEHOLDERS = new ConfigBooleanHotkeyed("replaceItemPredicatesWithPlaceholders", false, "", """
-		When loading a schematic, replaces each Item Predicate with its stored placeholder, if present.""");
-	public static final ConfigBooleanHotkeyed FORCE_SCHEMATIC_ITEM_OVERLAY = new ConfigBooleanHotkeyed("forceSchematicItemOverlay", false, "", KeybindSettings.GUI, """
-		Overwrite each slot in the opened container to show the schematic item instead of the real one.""");
+		§7Also adds the present components to the lore!"""
+	);
+	public static final ConfigBooleanHotkeyed REPLACE_ITEM_PREDICATES_WITH_PLACEHOLDERS = new ConfigBooleanHotkeyed(
+			"replaceItemPredicatesWithPlaceholders",
+			false,
+			"",
+			"""
+		When loading a schematic, replaces each Item Predicate with its stored placeholder, if present."""
+	);
+	public static final ConfigBooleanHotkeyed FORCE_SCHEMATIC_ITEM_OVERLAY = new ConfigBooleanHotkeyed(
+			"forceSchematicItemOverlay",
+			false,
+			"",
+			KeybindSettings.GUI,
+			"""
+		Overwrite each slot in the opened container to show the schematic item instead of the real one."""
+	);
 
 	public LitematicConfigs() {
 		super(ImmutableList.of(
--- src/main/java/dev/kikugie/techutils/config/MiscConfigs.java
+++ src/main/java/dev/kikugie/techutils/config/MiscConfigs.java
@@ -7,12 +7,23 @@ import fi.dy.masa.malilib.config.options.ConfigHotkey;
 import fi.dy.masa.malilib.config.options.ConfigInteger;
 
 public class MiscConfigs extends Configs.BaseConfigs {
-	public static final ConfigHotkey OPEN_CONFIG = new ConfigHotkey("openConfig", "U, C",
-		"Opens this screen if none other is open");
-	public static final ConfigBooleanHotkeyed COMPACT_SCOREBOARD = new ConfigBooleanHotkeyed("compactScoreboard", false, "F6", """
+	public static final ConfigHotkey OPEN_CONFIG = new ConfigHotkey(
+			"openConfig",
+			"U,C",
+			"Opens this screen if none other is open"
+	);
+	public static final ConfigBooleanHotkeyed COMPACT_SCOREBOARD = new ConfigBooleanHotkeyed(
+			"compactScoreboard",
+			false,
+			"F6",
+			"""
 		Show scoreboard values in compact notation.
-		For example: 123456 -> 123.4K""");
-	public static final ConfigHotkey GIVE_FULL_INV = new ConfigHotkey("giveFullInv", "G", """
+		For example: 123456 -> 123.4K"""
+	);
+	public static final ConfigHotkey GIVE_FULL_INV = new ConfigHotkey(
+			"giveFullInv",
+			"G",
+			"""
 		Give full inventory of an item with following rules:
 		- Main hand: item; Off hand: none; Result: shulker box of item.
 		- Main hand: item; Off hand: container; Result: container of item.
@@ -21,15 +32,36 @@ public class MiscConfigs extends Configs.BaseConfigs {
 		- Main hand: empty shulker box; Off hand: container; Result: container of stacked boxes.
 		- Main hand: shulker box; Off hand: none: Result: chest of full boxes.
 		- Main hand: shulker box; Off hand: container; Result: container of full boxes.
-		""");
-	public static final ConfigInteger BUNDLE_FILL = new ConfigInteger("bundleFill", 1, 1, 100, true,
-		"Amount of stacks to put in a bundle when using giveFullInv feature");
-	public static final ConfigBoolean FILL_SAFETY = new ConfigBoolean("fillSafety", true, """
+		"""
+	);
+	public static final ConfigInteger BUNDLE_FILL = new ConfigInteger(
+			"bundleFill",
+			1,
+			1,
+			100,
+			true,
+			"Amount of stacks to put in a bundle when using giveFullInv feature"
+	);
+	public static final ConfigBoolean FILL_SAFETY = new ConfigBoolean(
+			"fillSafety",
+			true,
+			"""
 		Restrict nested containers to prevent crashing yourself.
-		Here be cats and scratches!""");
-	public static final ConfigHotkey SCAN_INVENTORY = new ConfigHotkey("scanInventory", "I", "");
-	public static final ConfigInteger REQUEST_TIMEOUT = new ConfigInteger("requestTimeout", 60, 1, 1000, false,
-		"Time in game ticks before request is considered failed");
+		Here be cats and scratches!"""
+	);
+	public static final ConfigHotkey SCAN_INVENTORY = new ConfigHotkey(
+			"scanInventory",
+			"I",
+			""
+	);
+	public static final ConfigInteger REQUEST_TIMEOUT = new ConfigInteger(
+			"requestTimeout",
+			60,
+			1,
+			1000,
+			false,
+			"Time in game ticks before request is considered failed"
+	);
 
 	public MiscConfigs() {
 		super(ImmutableList.of(
